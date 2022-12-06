7c1feb9dee2da651dfd23a28b437698d387f3338
Akhi-B <Akhiabedin112416@gmail.com>
2022-12-05 21:55:58 -0500

    committing rest of the project

--- src/com/qa/fifa/SearchTest.java
+++ src/com/qa/fifa/SearchTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-
 import io.github.bonigarcia.wdm.WebDriverManager; 
 
 
@@ -33,7 +31,7 @@ public class SearchTest {
 		WebElement firstLink = driver.findElement(By.xpath("(//div[@class='ofy7ae'])[1]"));
 		
 		String actualText = firstLink.getText();
-		String expected = "FIFA World Cup Qatar 2023™";
+		String expected = "FIFA World Cup Qatar 2022™";
 		Assert.assertTrue(actualText.equalsIgnoreCase(expected), "Expected " + expected + " but found" + actualText);
 		
 		//Assert.assertTrue(actualText.equalsIgnoreCase(expected), "Expected " + expected + " but found " + actualText); 
